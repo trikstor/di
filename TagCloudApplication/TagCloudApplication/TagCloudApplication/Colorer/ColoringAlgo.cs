7bbe0a92d3eb57cac60103d89423f766bee3b37f
promo\Acer <azpk360@yandex.ru>
2017-12-03 12:26:55 +0500

    Доработка.

---
+++ TagCloudApplication/TagCloudApplication/TagCloudApplication/Colorer/ColoringAlgo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagCloudApplication.Coloring
+{
+    public class ColoringAlgo : IColorer
+    {
+        private Random Random = new Random();
+        private List<Brush> AllBrushes { get; }
+
+        public ColoringAlgo(Config config)
+        {
+            AllBrushes = config.CloudBrushes;
+        }
+        
+        public Brush GetColor(string words)
+        {
+            return AllBrushes[Random.Next(0, AllBrushes.Count)];
+        }
+    }
+}
