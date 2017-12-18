2921ee9567a2380da24febcd8ef79facd3310d47
promo\Acer <azpk360@yandex.ru>
2017-12-18 10:34:58 +0500

    правки

---
+++ TagCloudApplication/TagCloudApplication/TagCloudApplication/SimpleTextFileReader.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+
+namespace TagCloudApplication
+{
+    public static class SimpleTextFileReader
+    {
+        public static Result<string> GetTextWords(string path)
+        {
+            var result = Result.Of(() => File.ReadAllText(path));
+            if(result.IsSuccess)
+                return result.Value.Length > 0 ? result : Result.Fail<string>("Пустой файл.");
+            return result;
+        }
+    }
+}
